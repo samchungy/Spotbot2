be89f5eb293ffa0d402d89f784876d72397ffd48
Sam Chung <schung2@deloitte.com.au>
2019-12-17 16:44:03 +1100

    Initialise Auth

--- server/components/spotify-api/initialise.js
+++ server/components/spotify-api/initialise.js
@@ -1,9 +1,12 @@
+const config = require('config');
 const SpotifyWebApi = require('spotify-web-api-node');
+const SPOTIFY_CLIENT_ID = config.get('spotify_api.client_id');
+const SPOTIFY_CLIENT_SECRET = config.get('spotify_api.client_secret');
 
 // Setting credentials can be done in the wrapper's constructor, or using the API object's setters.
 var spotifyApi = new SpotifyWebApi({
-    clientId: process.env.SPOTIFY_CLIENT_ID,
-    clientSecret: process.env.SPOTIFY_CLIENT_SECRET
+    clientId: SPOTIFY_CLIENT_ID,
+    clientSecret: SPOTIFY_CLIENT_SECRET,
 });
 
 module.exports = spotifyApi
