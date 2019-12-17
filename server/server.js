1d0529030215888f06c644602b0b64502851e33e
Sam Chung <schung2@deloitte.com.au>
2019-12-17 16:36:57 +1100

    Require server config module

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 const serverless = require('serverless-http');
+const config = require('config');
 const Koa = require('koa');
 const Router = require('koa-router');
 const bodyParser = require('koa-bodyparser');
