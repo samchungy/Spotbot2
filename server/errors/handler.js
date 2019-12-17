3a7587500ee878e389ec7c9002562ba4cb71cd25
Sam Chung <schung2@deloitte.com.au>
2019-12-17 16:42:57 +1100

    Add new pino logger to error handler

--- server/errors/handler.js
+++ server/errors/handler.js
@@ -1,8 +1,10 @@
+const logger = require('pino')()
+
 module.exports = async (ctx, next) => {
     try {
         await next();
     } catch (err) {
-        console.log(err)
+        logger.error(err);
         ctx.status = err.status || 500;
         ctx.body = err.message;
     }
