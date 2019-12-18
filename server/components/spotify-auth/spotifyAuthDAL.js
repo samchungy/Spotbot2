f962ce4ba78278c4cf825fbe6a1953d6a245f0d2
Sam Chung <schung2@deloitte.com.au>
2019-12-18 15:49:19 +1100

    add state, token db retrieval

--- server/components/spotify-auth/spotifyAuthDAL.js
+++ server/components/spotify-auth/spotifyAuthDAL.js
@@ -2,7 +2,9 @@ const config = require('config');
 const logger = require('pino')();
 
 const { getSetting, putSetting, settingModel } = require('../../db/settings');
-const STATE = config.get('dynamodb.settings.state')
+const STATE = config.get('dynamodb.settings.state');
+const ACCESS = config.get('dynamodb.settings.access');
+const REFRESH = config.get('dynamodb.settings.refresh');
 
 async function storeState(state){
     let setting = settingModel(STATE, state);
@@ -10,12 +12,38 @@ async function storeState(state){
 }
 
 async function getState(){
-    let setting = settingModel(STATE, null);
-    delete setting.value;
-    return await getSetting(setting);
+    try {
+        let setting = settingModel(STATE, null);
+        let result = await getSetting(setting);
+        return result.Item.value;
+    } catch (error) {
+        logger.error("Get Setting failed");
+        throw error;
+    }
+}
+
+async function getTokens(){
+    try {
+        let access = settingModel(ACCESS, null);
+        let refresh = settingModel(REFRESH, null)
+        let result = await Promise.all([await getSetting(access), await getSetting(refresh)]);
+        return {access_token: result[0].Item.value, refresh_token: result[1].Item.value }
+    } catch (error) {
+        logger.error("Get Setting failed");
+        throw error;
+    }
+}
+
+
+async function storeTokens(access_token, refresh_token){
+    let access = settingModel(ACCESS, access_token);
+    let refresh = settingModel(REFRESH, refresh_token);
+    return await Promise.all([await putSetting(access), await putSetting(refresh)])
 }
 
 module.exports = {
     getState,
-    storeState
+    getTokens,
+    storeState,
+    storeTokens
 }
