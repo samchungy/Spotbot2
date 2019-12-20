52f93818e177281e186be015d358a6f0b6cafb1a
Sam Chung <schung2@deloitte.com.au>
2019-12-20 11:27:05 +1100

    Change auth path to /settings

--- server/components/spotify-auth/route.js
+++ server/components/spotify-auth/route.js
@@ -1,16 +1,11 @@
 const logger = require('pino')()
-const { getAuthorizationURL, validateAuthCode } = require('./spotifyAuth');
+const { validateAuthCode } = require('./spotifyAuth');
 
 module.exports = ( prefix, Router ) => {
     const router = new Router({
         prefix: prefix
     });
     router
-    .get('/', async (ctx, next) => {
-      //TODO ADD STATE (SLACK TRIGGER ID)
-      let auth_url = await getAuthorizationURL("ABC");
-      ctx.body = auth_url;
-    })
     .get('/callback', async (ctx, next) => {
       let { success, failReason } = await validateAuthCode(ctx.query.code, ctx.query.state);
       if (success){
