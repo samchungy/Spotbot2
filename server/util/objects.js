08df4421063f5406a9719884ea8d05d678dd5ab0
Sam Chung <schung2@deloitte.com.au>
2019-12-26 21:39:34 +1100

    Add deep equal for object equality

--- server/util/objects.js
+++ server/util/objects.js
@@ -1,3 +1,5 @@
+const { deepEqual } = require('fast-equals')
+
 function isEmpty(obj){
     return Object.entries(obj).length === 0 && obj.constructor === Object
 }
@@ -10,7 +12,12 @@ function nullOrValue(obj){
     }
 }
 
+function isEqual(obj1, obj2){
+    return deepEqual(obj1, obj2);
+}
+
 module.exports = {
     nullOrValue,
+    isEqual,
     isEmpty
 }
