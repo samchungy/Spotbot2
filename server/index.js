9b5fd99a6217cbe3aa23e59dc8bbdbfa35b6fa89
Sam Chung <schung2@deloitte.com.au>
2019-12-27 14:01:04 +1100

    Change settings router name

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 
 const slackRouter = require('./components/slack/route');
-const settingRouter = require('./components/settings/route');
+const settingsRouter = require('./components/settings/route');
 const spotifyAuthRouter = require('./components/spotify-auth/route')
 const controlRouter = require('./components/control/route');
 const tracksRouter = require('./components/tracks/route');
@@ -9,14 +9,14 @@ module.exports = ({Router}) => {
     const router = new Router();
 
     const slackRoute = slackRouter('/slack/actions', Router);
-    const adminRoute = settingRouter('/settings', Router);
+    const settingsRoute = settingsRouter('/settings', Router);
     const controlRoute = controlRouter('/control', Router);
     const tracksRoute = tracksRouter('/tracks', Router);
     const spotifyAuthRoute = spotifyAuthRouter('/auth', Router);
     
     router
         .use(slackRoute.routes())
-        .use(adminRoute.routes())
+        .use(settingsRoute.routes())
         .use(controlRoute.routes())
         .use(tracksRoute.routes())
         .use(spotifyAuthRoute.routes())
