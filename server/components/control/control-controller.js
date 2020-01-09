dab6558b110cda390eb4ca7c000a913fc1970ee0
Sam Chung <schung2@deloitte.com.au>
2020-01-09 15:10:33 +1100

    Pause functionality

--- server/components/control/control-controller.js
+++ server/components/control/control-controller.js
@@ -47,6 +47,7 @@ async function updatePanel(responseUrl, response, status) {
     if (!status) {
       status = await fetchCurrentPlayback();
     }
+    console.log(status);
     const {altText, currentPanel} = getCurrentTrackPanel(status, response);
 
     const controlPanel = [
@@ -61,8 +62,8 @@ async function updatePanel(responseUrl, response, status) {
     );
   } catch (error) {
     logger.error(error);
-      }
-    }
+  }
+}
 
 /**
  * Hits Play on Spotify
@@ -74,157 +75,41 @@ async function play(responseUrl, channelId) {
     const {success, response, status} = await setPlay();
     if (!success) {
       await updatePanel(responseUrl, response, status);
-  } else {
-      await updatePanel(responseUrl, null, status);
-      await post(
+    } else {
+      updatePanel(responseUrl, null, status);
+      post(
           inChannelPost(channelId, response, null),
       );
-  }
+    }
   } catch (error) {
     logger.error(error);
-}
+  }
 }
 
 /**
- * Returns a set of buttons for Slack
- * @return {Object} Slack Button Controls
+ * Hits pause on Spotify
+ * @param {string} responseUrl
+ * @param {string} channelId
+ * @param {string} userId
  */
-function getControls() {
-  const overflowOptions = [
-    overflowOption(`:put_litter_in_its_place: Reset playlist`, CONTROLS.reset),
-    overflowOption(`:put_litter_in_its_place: Clear Songs > 1 Day Old`, CONTROLS.clear_one),
-    overflowOption(`:leftwards_arrow_with_hook: Jump to Start of Playlist`, CONTROLS.jump_to_start),
-    overflowOption(`:twisted_rightwards_arrows: Toggle Shuffle`, CONTROLS.shuffle),
-    overflowOption(`:repeat: Toggle Repeat`, CONTROLS.repeat),
-  ];
-  const elements = [
-    buttonActionElement(`:arrow_forward: Play`, CONTROLS.play),
-    buttonActionElement(`:double_vertical_bar: Pause`, CONTROLS.pause),
-    buttonActionElement(`:black_right_pointing_double_triangle_with_vertical_bar: Skip`, CONTROLS.skip),
-    overflowActionElement(CONTROLLER_OVERFLOW, overflowOptions),
-  ];
-  return actionSection(CONTROLLER, elements);
+async function pause(responseUrl, channelId, userId) {
+  try {
+    const {success, response, status} = await setPause(userId);
+    if (!success) {
+      await updatePanel(responseUrl, response, status);
+    } else {
+      updatePanel(responseUrl, null, status);
+      post(
+          inChannelPost(channelId, response, null),
+      );
+    }
+  } catch (error) {
+    logger.error(error);
+  }
 }
-
-// {
-//   "blocks": [{
-//       "type": "section",
-//       "text": {
-//         "type": "mrkdwn",
-//         "text": "_Currently Playing_:\n<http://www.foo.com|*Magnolia*> \n *Artist:* Gang of Youths\n *Album*: Yadyadayada\n"
-//       },
-//       "accessory": {
-//         "type": "image",
-//         "image_url": "https://api.slack.com/img/blocks/bkb_template_images/plants.png",
-//         "alt_text": "plants"
-//       }
-//     },
-//     {
-//       "type": "context",
-//       "elements": [{
-//         "type": "mrkdwn",
-//         "text": ":information_source: Currently at song 7/20 on the Spotbot Playlist"
-//       }]
-//     },
-//     {
-//       "type": "context",
-//       "elements": [{
-//         "type": "mrkdwn",
-//         "text": ":warning: Shuffle *Enabled*, Repeat *Enabled*"
-//       }]
-//     },
-//     {
-//       "type": "actions",
-//       "elements": [{
-//           "type": "button",
-//           "text": {
-//             "type": "plain_text",
-//             "emoji": true,
-//             "text": ":arrow_forward: Play"
-//           },
-//           "value": "click_me_123"
-//         },
-//         {
-//           "type": "button",
-//           "text": {
-//             "type": "plain_text",
-//             "emoji": true,
-//             "text": ":double_vertical_bar: Pause"
-//           },
-//           "value": "click_me_123"
-//         },
-//         {
-//           "type": "button",
-//           "text": {
-//             "type": "plain_text",
-//             "emoji": true,
-//             "text": ":black_right_pointing_double_triangle_with_vertical_bar: Skip"
-//           },
-//           "value": "click_me_123"
-//         },
-//         {
-//           "type": "overflow",
-//           "options": [{
-//               "text": {
-//                 "type": "plain_text",
-//                 "text": ":put_litter_in_its_place: Reset playlist"
-//               },
-//               "value": "value-1"
-//             },
-//             {
-//               "text": {
-//                 "type": "plain_text",
-//                 "text": ":put_litter_in_its_place: Clear Songs > 1 Day Old"
-//               },
-//               "value": "value-1"
-//             },
-//             {
-//               "text": {
-//                 "type": "plain_text",
-//                 "text": ":leftwards_arrow_with_hook: Jump to Start of Playlist"
-//               },
-//               "value": "value-3"
-//             },
-//             {
-//               "text": {
-//                 "type": "plain_text",
-//                 "text": ":twisted_rightwards_arrows: Toggle Shuffle"
-//               },
-//               "value": "value-1"
-//             },
-//             {
-//               "text": {
-//                 "type": "plain_text",
-//                 "text": ":repeat: Toggle Repeat"
-//               },
-//               "value": "value-4"
-//             }
-//           ],
-//           "action_id": "overflow",
-//           "confirm": {
-//             "title": {
-//               "type": "plain_text",
-//               "text": "Are you sure?"
-//             },
-//             "text": {
-//               "type": "mrkdwn",
-//               "text": "Please make sure everyone is okay with you doing this."
-//             },
-//             "confirm": {
-//               "type": "plain_text",
-//               "text": "Do it"
-//             },
-//             "deny": {
-//               "type": "plain_text",
-//               "text": "Cancel"
-//             }
-//           }
-//         }
-//       ]
-//     }
-//   ]
-// }
 
 module.exports = {
   openControls,
+  pause,
+  play,
 };
