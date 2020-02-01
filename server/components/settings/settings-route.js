3320abd43d1ad3c9d8ace2e45b4a3f0f893825f0
Sam Chung <schung2@deloitte.com.au>
2020-02-01 16:00:23 +1100

    Add null check to route

--- server/components/settings/settings-route.js
+++ server/components/settings/settings-route.js
@@ -12,33 +12,35 @@ module.exports = ( prefix, Router ) => {
   router
       .post('/', async (ctx, next) => {
         const payload = ctx.request.body;
-        const textSplit = payload.text.split(' ');
-        if (textSplit.length == 0) {
-        // Implement General Spotbot Help Menu
-        } else {
-          switch (textSplit[0]) {
-            case 'settings':
-              if (!await isSetup(payload.team_id, payload.channel_id) || await checkIsAdmin(payload.team_id, payload.channel_id, payload.user_id, payload.response_url)) {
-                openSettings(payload.team_id, payload.channel_id, payload.trigger_id);
-              }
-              ctx.body = '';
-              break;
-            case 'blacklist':
-              if (await checkSettings(payload.team_id, payload.channel_id, payload.response_url) && await checkIsAdmin(payload.team_id, payload.channel_id, payload.user_id, payload.response_url)) {
-                openBlacklistModal(payload.team_id, payload.channel_id, payload.trigger_id);
-              }
-              ctx.body = '';
-              break;
-            case 'help':
-              ctx.body = HELP;
-              break;
+        if (payload.text) {
+          const textSplit = payload.text.split(' ');
+          if (textSplit.length == 0) {
+          // Implement General Spotbot Help Menu
+          } else {
+            switch (textSplit[0]) {
+              case 'settings':
+                if (!await isSetup(payload.team_id, payload.channel_id) || await checkIsAdmin(payload.team_id, payload.channel_id, payload.user_id, payload.response_url)) {
+                  openSettings(payload.team_id, payload.channel_id, payload.trigger_id);
+                }
+                ctx.body = '';
+                break;
+              case 'blacklist':
+                if (await checkSettings(payload.team_id, payload.channel_id, payload.response_url) && await checkIsAdmin(payload.team_id, payload.channel_id, payload.user_id, payload.response_url)) {
+                  openBlacklistModal(payload.team_id, payload.channel_id, payload.trigger_id);
+                }
+                ctx.body = '';
+                break;
+              case 'help':
+                ctx.body = HELP;
+                break;
+            }
           }
         }
       })
       .get('/auth/callback', async (ctx, next) => {
         const {success, failReason, state} = await validateAuthCode(ctx.query.code, ctx.query.state);
         if (success) {
-          ctx.body = 'Auth Success';
+          ctx.body = 'Authentication Successful. Please close this window';
           updateView(state.teamId, state.channelId);
         } else {
           ctx.status = 401;
