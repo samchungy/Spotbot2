f48a7b0f413758af3c50e9b11ac1aceb6107f1c7
Sam Chung <schung2@deloitte.com.au>
2020-02-06 21:17:24 +1100

    add artist test

--- __tests__/mocks/payloads/slash-commands.js
+++ __tests__/mocks/payloads/slash-commands.js
@@ -112,7 +112,23 @@ const removeTrackSlashPayload = {
 };
 
 
+const artistSlashPayload = {
+  token: '6r2mZJdBz8Gb8wSl49SHMABa',
+  team_id: 'TRVUTD7DM',
+  team_domain: 'spotbottest',
+  channel_id: 'CRVUTDP47',
+  channel_name: 'slack-bots',
+  user_id: 'URVUTD7UP',
+  user_name: 'samchungy',
+  command: '/artist',
+  text: '',
+  response_url: 'https://hooks.slack.com/commands/TRVUTD7DM/920606799857/zpbRZv0e10qbT5lqdmdwPEXK',
+  trigger_id: '920606799937.879979449463.03cf1e98affb1769fde3a2f3393ff4d3',
+};
+
+
 module.exports = {
+  artistSlashPayload,
   currentSlashPayload,
   findSlashPayload,
   pauseSlashPayload,
