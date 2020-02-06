b6e2baf8e9692555de4522093d1193e9d6569e13
Sam Chung <schung2@deloitte.com.au>
2020-02-07 00:08:26 +1100

    add blacklist test

--- __tests__/mocks/payloads/slash-commands.js
+++ __tests__/mocks/payloads/slash-commands.js
@@ -126,10 +126,40 @@ const artistSlashPayload = {
   trigger_id: '920606799937.879979449463.03cf1e98affb1769fde3a2f3393ff4d3',
 };
 
+const blacklistSlashPayload = {
+  token: '6r2mZJdBz8Gb8wSl49SHMABa',
+  team_id: 'TRVUTD7DM',
+  team_domain: 'spotbottest',
+  channel_id: 'CRVUTDP47',
+  channel_name: 'slack-bots',
+  user_id: 'URVUTD7UP',
+  user_name: 'samchungy',
+  command: '/spotbot',
+  text: 'blacklist',
+  response_url: 'https://hooks.slack.com/commands/TRVUTD7DM/920606799857/zpbRZv0e10qbT5lqdmdwPEXK',
+  trigger_id: '920606799937.879979449463.03cf1e98affb1769fde3a2f3393ff4d3',
+};
+
+const deviceSlashPayload = {
+  token: '6r2mZJdBz8Gb8wSl49SHMABa',
+  team_id: 'TRVUTD7DM',
+  team_domain: 'spotbottest',
+  channel_id: 'CRVUTDP47',
+  channel_name: 'slack-bots',
+  user_id: 'URVUTD7UP',
+  user_name: 'samchungy',
+  command: '/spotbot',
+  text: 'device',
+  response_url: 'https://hooks.slack.com/commands/TRVUTD7DM/920606799857/zpbRZv0e10qbT5lqdmdwPEXK',
+  trigger_id: '920606799937.879979449463.03cf1e98affb1769fde3a2f3393ff4d3',
+};
+
 
 module.exports = {
   artistSlashPayload,
+  blacklistSlashPayload,
   currentSlashPayload,
+  deviceSlashPayload,
   findSlashPayload,
   pauseSlashPayload,
   playSlashPayload,
