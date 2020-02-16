be27b3b58b3e149ccd377bf444d8b93e9e4479f2
Sam Chung <schung2@deloitte.com.au>
2020-02-16 15:40:25 +1100

    Add authentication callback

--- router/src/auth/auth-routes.js
+++ router/src/auth/auth-routes.js
@@ -1,7 +1,7 @@
+const Lambda = require('aws-sdk/clients/lambda');
+const lambda = new Lambda();
 const SNS = require('aws-sdk/clients/sns');
 const sns = new SNS();
-
-const {validateAuthCode} = require('/opt/spotify/spotify-auth/spotifyauth-controller');
 
 module.exports = ( prefix, Router ) => {
   const router = new Router({
@@ -9,15 +9,19 @@ module.exports = ( prefix, Router ) => {
   });
 
   router.get('/', async (ctx, next) => {
-    ctx.body = 'test';
-    const {success, failReason, state} = await validateAuthCode(ctx.query.code, ctx.query.state);
+    const params = {
+      FunctionName: process.env.SETTINGS_AUTH_VALIDATE, // the lambda function we are going to invoke
+      Payload: JSON.stringify({code: ctx.query.code, state: ctx.query.state}),
+    };
+    const {Payload: payload} = await lambda.invoke(params).promise();
+    const {success, failReason, state} = JSON.parse(payload);
     if (success) {
-      ctx.body = 'Authentication Successful. Please close this window';
       const params = {
         Message: JSON.stringify({teamId: state.teamId, channelId: state.channelId}),
         TopicArn: process.env.SETTINGS_UPDATE_VIEW,
       };
-      await sns.publish(params).promise(); ;
+      await sns.publish(params).promise();
+      ctx.body = 'Authentication Successful. Please close this window';
     } else {
       ctx.status = 401;
       ctx.body = failReason;
