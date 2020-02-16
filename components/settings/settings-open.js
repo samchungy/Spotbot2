6c3c4910b35e79ed6c283fb2b6ba1c803ab900ea
Sam Chung <schung2@deloitte.com.au>
2020-02-16 22:31:57 +1100

    Add fail to open settings message

--- components/settings/settings-open.js
+++ components/settings/settings-open.js
@@ -1,16 +1,21 @@
 const config = require(process.env.CONFIG);
 const logger = require(process.env.LOGGER);
 
-const {sendModal} = require('/opt/slack/slack-api');
+const {postEphemeral, sendModal} = require('/opt/slack/slack-api');
 const {slackModal} = require('/opt/slack/format/slack-format-modal');
+const {ephemeralPost} = require('/opt/slack/format/slack-format-reply');
 const {getAuthBlock} = require('/opt/settings-blocks/settings-auth-blocks');
 const {getSettingsBlocks} = require('/opt/settings-blocks/settings-blocks');
 
 const SETTINGS_MODAL = config.slack.actions.settings_modal;
 
+const OPEN_RESPONSE = {
+  fail: ':x: Something went wrong! Could not open Settings. Please try again.',
+};
+
 module.exports.handler = async (event, context) => {
+  const {teamId, channelId, triggerId, userId} = JSON.parse(event.Records[0].Sns.Message);
   try {
-    const {teamId, channelId, triggerId} = JSON.parse(event.Records[0].Sns.Message);
     const {authBlock, authError} = await getAuthBlock(teamId, channelId, triggerId);
     // Do not load settings blocks if Spotify is not authenticated
     const blocks = [
@@ -23,5 +28,13 @@ module.exports.handler = async (event, context) => {
   } catch (error) {
     logger.error('Open Settings Failed');
     logger.error(error);
+    try {
+      await postEphemeral(
+          ephemeralPost(channelId, userId, OPEN_RESPONSE.fail, null),
+      );
+    } catch (error) {
+      logger.error('Failed to report open settings failed');
+      logger.error(error);
+    }
   }
 };
