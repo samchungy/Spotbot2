264c0bf04914590a2ca6c96b6d3ceecafd42670c
Sam Chung <schung2@deloitte.com.au>
2020-02-29 18:08:51 +1100

    add track removal route

--- router/src/tracks/tracks-route.js
+++ router/src/tracks/tracks-route.js
@@ -1,12 +1,17 @@
 const SNS = require('aws-sdk/clients/sns');
 const sns = new SNS();
+
+const config = require(process.env.CONFIG);
+const {openModal} = require('../response/open-modal');
 const {publicAck} = require('/opt/slack/format/slack-format-reply');
 const {checkSettingsMiddleware} = require('../../middleware/settings-middleware');
 
 const TRACKS_CURRENT = process.env.SNS_PREFIX + 'tracks-current';
 const TRACKS_FIND_ARTISTS_SEARCH = process.env.SNS_PREFIX + 'tracks-find-artists-search';
 const TRACKS_FIND_SEARCH = process.env.SNS_PREFIX + 'tracks-find-search';
+const TRACKS_REMOVE_OPEN = process.env.SNS_PREFIX + 'tracks-remove-open';
 const TRACKS_WHOM = process.env.SNS_PREFIX + 'tracks-whom';
+const EMPTY_MODAL = config.slack.actions.empty_modal;
 
 module.exports = ( prefix, Router ) => {
   const router = new Router({
@@ -53,11 +58,17 @@ module.exports = ( prefix, Router ) => {
         };
         await sns.publish(params).promise();
         ctx.body = publicAck('');
+      })
+      .post('/remove', async (ctx, next) => {
+        const payload = ctx.request.body;
+        const settings = ctx.state.settings;
+        const removePayload = await openModal(payload.team_id, payload.channel_id, payload.trigger_id, EMPTY_MODAL, 'Remove Tracks', 'Confirm', 'Cancel');
+        params = {
+          Message: JSON.stringify({teamId: payload.team_id, channelId: payload.channel_id, settings: settings, viewId: removePayload.view.id, userId: payload.user_id}),
+          TopicArn: TRACKS_REMOVE_OPEN,
+        };
+        await sns.publish(params).promise();
+        ctx.body = publicAck('');
       });
-  // .post('/remove', async (ctx, next) => {
-  //   const payload = ctx.request.body;
-  //   removeTrackReview(payload.team_id, payload.channel_id, payload.user_id, payload.trigger_id);
-  //   ctx.body = publicAck('');
-  // });
   return router;
 };
