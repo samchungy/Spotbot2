a1d5daec5900842e3b463baec7cf40dc85a088c8
Sam Chung <schung2@deloitte.com.au>
2020-03-02 18:36:15 +1100

    Update authorizer, remove dependancy

--- router/middleware/authorizer-body.js
+++ router/middleware/authorizer-body.js
@@ -1,12 +1,11 @@
 const crypto = require('crypto');
-const qs = require('qs');
 // fetch this from environment variables
 const SLACK_SIGNING_SECRET = process.env.SLACK_SIGNING_SECRET;
 
 const signVerification = async (ctx, next) => {
   const slackSignature = ctx.headers['x-slack-signature'];
   const timestamp = ctx.headers['x-slack-request-timestamp'];
-  const requestBody = qs.stringify(ctx.request.body, {format: 'RFC1738'});
+  const requestBody = ctx.request.rawBody;
   const sigBasestring = 'v0:' + timestamp + ':' + requestBody;
   const mySignature = 'v0=' +
                   crypto.createHmac('sha256', SLACK_SIGNING_SECRET)
