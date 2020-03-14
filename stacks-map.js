e1e8d8bfdd32c64201e0430af6b176afa5638627
Sam Chung <schung2@deloitte.com.au>
2020-03-14 16:34:17 +1100

    Change stacks

--- stacks-map.js
+++ stacks-map.js
@@ -1,8 +1,19 @@
 module.exports = (resource, logicalId) => {
-  if (logicalId.startsWith('Router')) return {destination: 'Router'};
-  if (logicalId.startsWith('Settings')) return {destination: 'Settings'};
-  if (logicalId.startsWith('Tracks')) return {destination: 'Tracks'};
-  if (logicalId.startsWith('Control')) return {destination: 'Control'};
-  if (logicalId.startsWith('Delete')) return {destination: 'Delete'};
+  logicalId = logicalId.toLowerCase();
+  if (logicalId.startsWith('router') || logicalId.startsWith('apigatewaymethod')) return {destination: 'Router'};
+  if (logicalId.startsWith('settings') || isSns('settings', logicalId)) return {destination: 'Settings'};
+  if (logicalId.startsWith('tracks') || isSns('tracks', logicalId)) return {destination: 'Tracks'};
+  if (logicalId.startsWith('control') || isSns('control', logicalId)) return {destination: 'Control'};
+  if (logicalId.startsWith('delete') || isSns('delete', logicalId)) return {destination: 'Delete'};
   // Falls back to default
 };
+
+/**
+ * Identifies SNS objects
+ * @param {string} identifier
+ * @param {string} logicalId
+ * @return {boolean}
+ */
+function isSns(identifier, logicalId) {
+  return (logicalId.startsWith('snstopic') && logicalId.includes(identifier));
+}
