f27832e84df544eb3e9810ab2ad1a02ad360ac9d
Sam Chung <schung2@deloitte.com.au>
2020-05-11 22:57:07 +1000

    Move router layer location

--- layers/layers-router-core/slack-modal.js
+++ layers/layers-router-core/slack-modal.js
@@ -1,5 +1,5 @@
 const logger = require(process.env.LOGGER);
-const {sendModal} = require('/opt/slack/slack-api');
+const {pushModal, sendModal} = require('/opt/slack/slack-api');
 const {textSection} = require('/opt/slack/format/slack-format-blocks');
 const {slackModal} = require('/opt/slack/format/slack-format-modal');
 
@@ -23,6 +23,27 @@ async function openModal(teamId, channelId, triggerId, callback, title, submit, 
   }
 }
 
+/**
+ *
+ * @param {*} teamId
+ * @param {*} channelId
+ * @param {*} triggerId
+ * @param {*} callback
+ * @param {*} title
+ * @param {*} submit
+ * @param {*} cancel
+ */
+async function pushView(teamId, channelId, triggerId, callback, title, submit, cancel) {
+  try {
+    const modal = slackModal(callback, title, submit, cancel, [textSection('Loading...')], false, channelId);
+    return await pushModal(modal, triggerId );
+  } catch (error) {
+    logger.error(error);
+    throw error;
+  }
+}
+
 module.exports = {
   openModal,
+  pushView,
 };
