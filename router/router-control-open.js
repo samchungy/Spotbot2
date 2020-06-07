46a567c95d90932a2882f5337ede8c4b0c311e13
Sam Chung <schung2@deloitte.com.au>
2020-06-07 13:14:01 +1000

    Refactor control router

--- router/router-control-open.js
+++ router/router-control-open.js
@@ -3,45 +3,43 @@ const qs = require('qs');
 const sns = require('/opt/sns');
 const logger = require(process.env.LOGGER);
 
+// Slack
 const slackAuthorized = require('/opt/authorizer');
-const {checkIsSetup} = require('/opt/check-settings');
 const {publicAck} = require('/opt/slack-reply');
+// Settings
+const {checkIsSetup} = require('/opt/check-settings');
+// Errors
+const {SetupError} = require('/opt/errors/errors-settings');
 
 const CONTROL_OPEN = process.env.SNS_PREFIX + 'control-open';
 
-module.exports.handler = async (event, context) => {
-  let statusCode = 200; let body = '';
-  try {
-    if (!slackAuthorized(event)) {
-      statusCode = 401;
-      body = 'Unauathorized';
-      return {
-        statusCode,
-        body,
-      };
-    }
-    const payload = qs.parse(event.body);
-    const {settings} = await checkIsSetup(payload.team_id, payload.channel_id);
-    if (!settings) {
-      body = ':information_source: Spotbot is not setup in this channel. Use `/spotbot settings` to setup Spotbot.';
-      return {
-        statusCode,
-        body,
-      };
-    }
-    const params = {
-      Message: JSON.stringify({teamId: payload.team_id, channelId: payload.channel_id, settings, timestamp: null}),
-      TopicArn: CONTROL_OPEN,
-    };
-    await sns.publish(params).promise();
-    body = JSON.stringify(publicAck(''));
-  } catch (error) {
-    logger.error('Open control router failed');
-    logger.error(error);
-    body = ':warning: An error occured. Please try again.';
-  }
-  return {
-    statusCode,
-    body,
+const router = async (event, context) => {
+  const payload = qs.parse(event.body);
+  const params = {
+    Message: JSON.stringify({
+      teamId: payload.team_id,
+      channelId: payload.channel_id,
+      settings: await checkIsSetup(payload.team_id, payload.channel_id),
+      userId: payload.user_id,
+    }),
+    TopicArn: CONTROL_OPEN,
   };
+  await sns.publish(params).promise();
+  return JSON.stringify(publicAck(''));
+};
+
+
+module.exports.handler = async (event, context) => {
+  if (!slackAuthorized(event)) {
+    return {statusCode: 401, body: 'Unauathorized'};
+  }
+  return await router(event, context)
+      .then((data) => ({statusCode: 200, body: data ? data: ''}))
+      .catch((error) => {
+        if (error instanceof SetupError) {
+          return {statusCode: 200, body: error.message};
+        }
+        logger.error(error, 'Uncategorized Error in the control open Router');
+        return {statusCode: 200, body: ':warning: An error occured. Please try again.'};
+      });
 };
--- router/router-control-skip.js
+++ router/router-control-skip.js
@@ -20,7 +20,6 @@ const router = async (event, context) => {
       teamId: payload.team_id,
       channelId: payload.channel_id,
       settings: await checkIsSetup(payload.team_id, payload.channel_id),
-      timestamp: null,
       userId: payload.user_id,
     }),
     TopicArn: CONTROL_SKIP_START,
@@ -35,12 +34,11 @@ module.exports.handler = async (event, context) => {
   }
   return await router(event, context)
       .then((data) => ({statusCode: 200, body: data ? data: ''}))
-      .catch((err) => {
-        if (err instanceof SetupError) {
-          return {statusCode: 200, body: err.message};
+      .catch((error) => {
+        if (error instanceof SetupError) {
+          return {statusCode: 200, body: error.message};
         }
-        logger.error('Uncategorized Error in Skip Router');
-        logger.error(err);
+        logger.error(error, 'Uncategorized Error in Skip Router');
         return {statusCode: 200, body: ':warning: An error occured. Please try again.'};
       });
 };
