697b3668437096a4b95cd0a634e2c9910b8339a3
Sam Chung <schung2@deloitte.com.au>
2020-06-07 13:14:54 +1000

    Refactor track routers

--- router/router-tracks-current.js
+++ router/router-tracks-current.js
@@ -1,48 +1,43 @@
 'use strict';
 const qs = require('qs');
 const sns = require('/opt/sns');
-
 const logger = require(process.env.LOGGER);
 
+// Slack
 const slackAuthorized = require('/opt/authorizer');
-const {checkIsSetup} = require('/opt/check-settings');
 const {publicAck} = require('/opt/slack-reply');
+// Settings
+const {checkIsSetup} = require('/opt/check-settings');
+// Errors
+const {SetupError} = require('/opt/errors/errors-settings');
 
 const TRACKS_CURRENT = process.env.SNS_PREFIX + 'tracks-current';
 
-module.exports.handler = async (event, context) => {
-  let statusCode = 200; let body = '';
-  try {
-    if (!slackAuthorized(event)) {
-      statusCode = 401;
-      body = 'Unauathorized';
-      return {
-        statusCode,
-        body,
-      };
-    }
-    const payload = qs.parse(event.body);
-    const {settings} = await checkIsSetup(payload.team_id, payload.channel_id);
-    if (!settings) {
-      body = ':information_source: Spotbot is not setup in this channel. Use `/spotbot settings` to setup Spotbot.';
-      return {
-        statusCode,
-        body,
-      };
-    }
-    const params = {
-      Message: JSON.stringify({teamId: payload.team_id, channelId: payload.channel_id, settings, userId: payload.user_id}),
-      TopicArn: TRACKS_CURRENT,
-    };
-    await sns.publish(params).promise();
-    body = JSON.stringify(publicAck(''));
-  } catch (error) {
-    logger.error('Current track router failed');
-    logger.error(error);
-    body = ':warning: An error occured. Please try again.';
-  }
-  return {
-    statusCode,
-    body,
+const router = async (event, context) => {
+  const payload = qs.parse(event.body);
+  const params = {
+    Message: JSON.stringify({
+      teamId: payload.team_id,
+      channelId: payload.channel_id,
+      settings: await checkIsSetup(payload.team_id, payload.channel_id),
+    }),
+    TopicArn: TRACKS_CURRENT,
   };
+  await sns.publish(params).promise();
+  return JSON.stringify(publicAck(''));
+};
+
+module.exports.handler = async (event, context) => {
+  if (!slackAuthorized(event)) {
+    return {statusCode: 401, body: 'Unauathorized'};
+  }
+  return await router(event, context)
+      .then((data) => ({statusCode: 200, body: data ? data: ''}))
+      .catch((error) => {
+        if (error instanceof SetupError) {
+          return {statusCode: 200, body: error.message};
+        }
+        logger.error(error, 'Uncategorized Error in Current Router');
+        return {statusCode: 200, body: ':warning: An error occured. Please try again.'};
+      });
 };
