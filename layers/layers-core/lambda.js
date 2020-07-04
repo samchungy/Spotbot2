e474ba44185494240f9257d5fddeb2b52879a342
Sam Chung <schung2@deloitte.com.au>
2020-07-04 14:02:05 +1000

    Update dependencies, local

--- layers/layers-core/lambda.js
+++ layers/layers-core/lambda.js
@@ -1,7 +1,7 @@
 const Lambda = require('aws-sdk/clients/lambda');
 const lambda = new Lambda(process.env.ENV == 'local' ?
   {
-    endpoint: 'http://localhost:4000',
+    endpoint: 'http://localhost:3002',
     accessKeyId: 'DEFAULT_ACCESS_KEY',
     secretAccessKey: 'DEFAULT_SECRET',
   } : {},
