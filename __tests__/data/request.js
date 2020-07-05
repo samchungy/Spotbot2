774e69650072391dd7e84147da207af2186a0f6e
Sam Chung <schung2@deloitte.com.au>
2020-07-05 23:40:34 +1000

    add mock data

---
+++ __tests__/data/request.js
@@ -0,0 +1,27 @@
+module.exports = {
+  teamId: 'TRVUTD7DM',
+  channelId: 'CRU3H4MEC',
+  settings: {
+    'default_device': {
+      'name': 'AU13282 - Computer',
+      'id': '87997bb4312981a00f1d8029eb874c55a211a0cc',
+    },
+    'playlist': {
+      'name': 'Test',
+      'id': '2nuwjAGCHQiPabqGH6SLty',
+      'uri': 'spotify:playlist:2nuwjAGCHQiPabqGH6SLty',
+      'url': 'https://open.spotify.com/playlist/2nuwjAGCHQiPabqGH6SLty',
+    },
+    'skip_votes': '0',
+    'timezone': 'Australia/Melbourne',
+    'skip_votes_ah': '0',
+    'channel_admins': [
+      'URVUTD7UP',
+    ],
+    'ghost_mode': 'true',
+    'back_to_playlist': 'true',
+    'disable_repeats_duration': '0',
+  },
+  viewId: 'V016R2YRZHS',
+  url: 'https://939f6b415fb5.ngrok.io/local',
+};
---
+++ __tests__/data/spotify/device.js
@@ -0,0 +1,18 @@
+module.exports = [
+  {
+    'devices': [
+      {
+        'id': '97667bb4312691a00f1d9026eb974c55a211a0cA',
+        'is_active': false,
+        'is_private_session': false,
+        'is_restricted': false,
+        'name': 'DESKTOP',
+        'type': 'Computer',
+        'volume_percent': 0,
+      },
+    ],
+  },
+  {
+    'devices': [],
+  },
+];
