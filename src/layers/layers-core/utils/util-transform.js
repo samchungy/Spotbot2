edd87cc52e0a8f0b66ad6a8846001b6a08d5ebc1
Sam Chung <schung2@deloitte.com.au>
2020-07-27 21:51:48 +1000

    lint fix

--- src/layers/layers-core/utils/util-transform.js
+++ src/layers/layers-core/utils/util-transform.js
@@ -6,7 +6,7 @@
  */
 function encode64(unencoded) {
   return Buffer.from(unencoded || '').toString('base64');
-};
+}
 
 /**
  * Decodes in Base64
@@ -15,7 +15,7 @@ function encode64(unencoded) {
  */
 function decode64(encoded) {
   return Buffer.from(encoded || '', 'base64').toString('utf8');
-};
+}
 
 module.exports = {
   decode64,
