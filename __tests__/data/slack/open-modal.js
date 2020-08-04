003638f273fe4f19c46dfb5ec37d0308b1fde885
Sam Chung <schung2@deloitte.com.au>
2020-08-04 19:30:45 +1000

    add mock data

---
+++ __tests__/data/slack/open-modal.js
@@ -0,0 +1,54 @@
+module.exports = {
+  0: {
+    'ok': true,
+    'view': {
+      'id': 'V0183BK45D3',
+      'team_id': 'TRVUTD7DM',
+      'type': 'modal',
+      'blocks': [
+        {
+          'type': 'section',
+          'block_id': 'EI2x',
+          'text': {
+            'type': 'mrkdwn',
+            'text': 'Loading...',
+            'verbatim': false,
+          },
+        },
+      ],
+      'private_metadata': 'CRU3H4MEC',
+      'callback_id': '',
+      'state': {
+        'values': {},
+      },
+      'hash': '1596362382.xougXRNo',
+      'title': {
+        'type': 'plain_text',
+        'text': 'Spotbot Settings',
+        'emoji': true,
+      },
+      'clear_on_close': false,
+      'notify_on_close': false,
+      'close': {
+        'type': 'plain_text',
+        'text': 'Cancel',
+        'emoji': true,
+      },
+      'submit': null,
+      'previous_view_id': null,
+      'root_view_id': 'V0183BK45D3',
+      'app_id': 'A012EC4351T',
+      'external_id': '',
+      'app_installed_team_id': 'TRVUTD7DM',
+      'bot_id': 'B012FPR625Q',
+    },
+    'response_metadata': {
+      'scopes': [
+        'commands',
+        'channels:read',
+        'groups:read',
+        'chat:write',
+      ],
+    },
+  },
+};
