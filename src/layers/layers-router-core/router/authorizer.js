57f1e3c7ed344b06e5bfd1d79f61a8ae1fe1b813
Sam Chung <schung2@deloitte.com.au>
2020-08-07 11:49:33 +1000

    add authorizer unit tests

--- src/layers/layers-router-core/router/authorizer.js
+++ src/layers/layers-router-core/router/authorizer.js
@@ -5,8 +5,8 @@ const SLACK_SIGNING_SECRET = process.env.SLACK_SIGNING_SECRET;
 const signVerification = (event) => {
   const slackSignature = event.headers['X-Slack-Signature'];
   const timestamp = event.headers['X-Slack-Request-Timestamp'];
-  const time = Math.floor(new Date().getTime()/1000);
-  if (Math.abs(time - timestamp) <= 300 && slackSignature) {
+  const time = Math.floor(Date.now()/1000);
+  if (!isNaN(timestamp) && Math.abs(time - timestamp) <= 300 && slackSignature) {
     const sigBasestring = 'v0:' + timestamp + ':' + event.body;
     const mySignature = 'v0=' +
                     crypto.createHmac('sha256', SLACK_SIGNING_SECRET)
