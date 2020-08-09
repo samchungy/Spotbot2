86a9a75fc46d7e51eee4f9d925c25a824b85435e
Sam Chung <schung2@deloitte.com.au>
2020-08-09 18:47:34 +1000

    add slack modal unit test

--- src/layers/layers-router-core/router/slack-modal.js
+++ src/layers/layers-router-core/router/slack-modal.js
@@ -1,28 +1,22 @@
-const logger = require('/opt/utils/util-logger');
 const {pushModal, sendModal} = require('/opt/slack/slack-api');
 const {textSection} = require('/opt/slack/format/slack-format-blocks');
 const {slackModal} = require('/opt/slack/format/slack-format-modal');
 const RESPONSE = {
   loading: 'Loading...',
-  open_failed: 'Opening Slack Modal Failed',
-  push_failed: 'Pushing Slack View Failed',
 };
 
-const openModal = async (teamId, channelId, triggerId, callback, title, submit, cancel) => {
-  const modal = slackModal(callback, title, submit, cancel, [textSection(RESPONSE.loading)], false, channelId);
-  return await sendModal(triggerId, modal).catch((error) => {
-    logger.error(error, RESPONSE.open_failed);
-  });
+const openModal = async (teamId, channelId, triggerId, callbackId, title, submit, cancel) => {
+  const modal = slackModal(callbackId, title, submit, cancel, [textSection(RESPONSE.loading)], false, channelId);
+  return await sendModal(triggerId, modal);
 };
 
-const pushView = async (teamId, channelId, triggerId, callback, title, submit, cancel) => {
-  const modal = slackModal(callback, title, submit, cancel, [textSection(RESPONSE.loading)], false, channelId);
-  return await pushModal(triggerId, modal).catch((error) => {
-    logger.error(error, RESPONSE.push_failed);
-  });
+const pushView = async (teamId, channelId, triggerId, callbackId, title, submit, cancel) => {
+  const modal = slackModal(callbackId, title, submit, cancel, [textSection(RESPONSE.loading)], false, channelId);
+  return await pushModal(triggerId, modal);
 };
 
 module.exports = {
   openModal,
   pushView,
+  RESPONSE,
 };
