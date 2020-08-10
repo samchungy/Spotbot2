850db1b34f049fbaf0569e202585ee862d726b90
Sam Chung <schung2@deloitte.com.au>
2020-08-10 22:59:44 +1000

    add settings device select unit tests

--- src/components/settings/settings-device-select.js
+++ src/components/settings/settings-device-select.js
@@ -11,21 +11,21 @@ const {fetchCurrentPlayback} = require('/opt/spotify/spotify-api-v2/spotify-api-
 const Device = require('/opt/spotify/spotify-objects/util-spotify-device');
 
 // Slack
-const {updateModal, postEphemeral} = require('/opt/slack/slack-api');
+const {updateModal} = require('/opt/slack/slack-api');
 const {textSection} = require('/opt/slack/format/slack-format-blocks');
 const {selectStatic, option, slackModal} = require('/opt/slack/format/slack-format-modal');
-const {ephemeralPost} = require('/opt/slack/format/slack-format-reply');
 const {reportErrorToSlack} = require('/opt/slack/slack-error-reporter');
 
 const DEVICE_MODAL = config.slack.actions.device_modal;
 const DEFAULT_DEVICE = config.dynamodb.settings.default_device;
 
-const DEVICE_RESPONSE = {
+const RESPONSE = {
   default: (deviceName) => `Spotbot will try to keep playing on the current device despite what the default device is set as in the settings. When Spotify is not reporting a device, Spotbot will attempt to fallback onto the default. To change the default, please go to \`/spotbot settings\`.\n\n *Current Default Device:* ${deviceName}`,
   fail: ':x: Something went wrong! Could not open devices menu. Please try again.',
   hint: 'The device which you will be playing music through.',
   no_device: `:information_source: No devices currently open.`,
   title: 'Select a device',
+  current: (name) => `Current Device: ${name}`,
 };
 
 /**
@@ -41,31 +41,26 @@ const getBlocks = async (teamId, channelId, auth, devices) => {
   const status = await fetchCurrentPlayback(auth);
 
   const blocks = [
-    textSection(DEVICE_RESPONSE.default(defaultDevice.name)),
-    ...status.device ? [getStatusBlock(status, devices)] : [getDeviceBlock(devices)],
+    textSection(RESPONSE.default(defaultDevice.name)),
+    getStatusBlock(status, devices),
   ];
   return blocks;
 };
 
-const getDeviceBlock = (devices) => {
-  const options = devices.map((device) => option(`${device.name} - ${device.type}`, device.id));
-  return selectStatic(DEVICE_MODAL, DEVICE_RESPONSE.title, DEVICE_RESPONSE.hint, null, options, null);
-};
-
 const getStatusBlock = (status, devices) => {
   const statusDevice = new Device(status.device);
-  const initial = option(`Current Device: ${statusDevice.name}`, statusDevice.id ? statusDevice.id : 'null');
+  const initial = option(RESPONSE.current(statusDevice.name), statusDevice.id || 'null');
   const options = [
     initial,
-    ...devices.filter((device) => device.id != status.device.id).map((device) => {
+    ...devices.filter((device) => device.id != statusDevice.id).map((device) => {
       const deviceObj = new Device(device);
       return option(deviceObj.name, deviceObj.id);
     }),
   ];
-  return selectStatic(DEVICE_MODAL, DEVICE_RESPONSE.title, DEVICE_RESPONSE.hint, initial, options, null);
+  return selectStatic(DEVICE_MODAL, RESPONSE.title, RESPONSE.hint, initial, options, null);
 };
 
-const openDeviceModal = async (teamId, channelId, userId, viewId) => {
+const main = async (teamId, channelId, userId, viewId) => {
   const auth = await authSession(teamId, channelId);
   const spotifyDevices = await fetchDevices(auth);
   if (spotifyDevices.devices.length) {
@@ -73,16 +68,18 @@ const openDeviceModal = async (teamId, channelId, userId, viewId) => {
     const modal = slackModal(DEVICE_MODAL, `Spotify Devices`, `Switch to Device`, `Cancel`, blocks, false, channelId);
     await updateModal(viewId, modal);
   } else {
-    const message = ephemeralPost(channelId, userId, DEVICE_RESPONSE.no_device, null);
-    await postEphemeral(message);
+    const blocks = [textSection(RESPONSE.no_device)];
+    const modal = slackModal(DEVICE_MODAL, `Spotify Devices`, null, `Close`, blocks, false, channelId);
+    await updateModal(viewId, modal);
   }
 };
 
 module.exports.handler = async (event, context) => {
   const {teamId, channelId, userId, viewId} = JSON.parse(event.Records[0].Sns.Message);
-  await openDeviceModal(teamId, channelId, userId, viewId)
+  await main(teamId, channelId, userId, viewId)
       .catch((error)=>{
-        logger.error(error, DEVICE_RESPONSE.failed);
-        reportErrorToSlack(teamId, channelId, userId, DEVICE_RESPONSE.failed);
+        logger.error(error, RESPONSE.failed);
+        reportErrorToSlack(teamId, channelId, userId, RESPONSE.failed);
       });
 };
+module.exports.RESPONSE = RESPONSE;
