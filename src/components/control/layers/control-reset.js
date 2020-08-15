8dbc7ae7df4ed1a358e21999b3f16bc6429358af
Sam Chung <schung2@deloitte.com.au>
2020-08-15 13:34:56 +1000

    remove try catch block

--- src/components/control/layers/control-reset.js
+++ src/components/control/layers/control-reset.js
@@ -1,6 +1,5 @@
 const moment = require('/opt/nodejs/moment-timezone/moment-timezone-with-data-1970-2030');
 const config = require('/opt/config/config');
-const logger = require('/opt/utils/util-logger');
 
 // Spotify
 const PlaylistTrack = require('/opt/spotify/spotify-objects/util-spotify-playlist-track');
@@ -15,22 +14,17 @@ const LIMIT = config.spotify_api.playlists.tracks.limit;
  * @param {Number} total
  */
 const getReviewTracks = async (auth, playlist, total) => {
-  try {
-    const timeBefore = moment().subtract(30, 'minutes');
-    const offset = Math.max(0, total-LIMIT);
-    const spotifyTracks = await fetchTracks(auth, playlist.id, null, offset);
-    const playlistTracks = spotifyTracks.items.map((track) => new PlaylistTrack(track)).reverse();
-    const reviewTracks = playlistTracks.reduce((review, track) => {
-      if (moment(track.addedAt).isAfter(timeBefore)) {
-        review.push(track);
-      }
-      return review;
-    }, []);
-    return reviewTracks;
-  } catch (error) {
-    logger.error('Getting Review Tracks failed.');
-    throw error;
-  }
+  const timeBefore = moment().subtract(30, 'minutes');
+  const offset = Math.max(0, total-LIMIT);
+  const spotifyTracks = await fetchTracks(auth, playlist.id, null, offset);
+  const playlistTracks = spotifyTracks.items.map((track) => new PlaylistTrack(track)).reverse();
+  const reviewTracks = playlistTracks.reduce((review, track) => {
+    if (moment(track.addedAt).isAfter(timeBefore)) {
+      review.push(track);
+    }
+    return review;
+  }, []);
+  return reviewTracks;
 };
 
 module.exports = {
