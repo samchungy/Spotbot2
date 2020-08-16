45ea07839edc22a265548a2eec4b1c174ab0d31f
Sam Chung <schung2@deloitte.com.au>
2020-08-16 13:29:07 +1000

    add reset review open unit test

--- src/components/control/control-reset-review-open.js
+++ src/components/control/control-reset-review-open.js
@@ -21,13 +21,21 @@ const RESET_MODAL = config.slack.actions.reset_modal;
 const REVIEW_JUMP = config.slack.actions.reset_review_jump;
 const PLAYLIST = config.dynamodb.settings.playlist;
 
-const RESET_RESPONSE = {
+const RESPONSE = {
   failed: 'Opening a reset review failed',
   empty: ':information_source: Playlist is already empty.',
   error: ':warning: An error occured.',
-  kept: (trackUris) => ` ${trackUris.length} ${trackUris.length > 1 ? `tracks` : `track`} from the past 30 minutes ${trackUris.length > 1 ? `were` : `was`} kept.`,
   review_title: (numTracks) => `*${numTracks}* ${numTracks > 1 ? `tracks were` : `track was`} added in the past 30 minutes. Are you sure you want to remove ${numTracks > 1 ? `them` : `it`}?  Closing this window will keep none.`,
-  success: (userId) => `:put_litter_in_its_place: The Spotbot playlist was reset by <@${userId}>`,
+};
+
+const LABELS = {
+  ten: `Added less than 10 minutes ago`,
+  twenty: `Added less than 20 minutes ago`,
+  thirty: `Added less than 30 minutes ago`,
+  select: `Select songs to keep on the playlist`,
+  preselect: `Tracks added in the past 10 minutes have been pre-selected.`,
+  jump: `Jump to the start of the playlist?`,
+  jump_desc: `This will only work if a track is selected above.`,
 };
 
 /**
@@ -36,67 +44,67 @@ const RESET_RESPONSE = {
  * @return {Object} reviewBlocks
  */
 const getReviewBlocks = (playlistTracks) => {
-  const buckets = {ten: [], twenty: [], thirty: []};
-  const initialOptions = [];
   const tenMinutes = moment().subtract(10, 'minutes');
   const twentyMinutes = moment().subtract(20, 'minutes');
   // Sort into time buckets
-  playlistTracks.forEach((track) => {
+  const buckets = playlistTracks.reduce((buckets, track) => {
     const op = option(track.title, track.uri);
     if (moment(track.addedAt).isSameOrAfter(tenMinutes)) {
-      initialOptions.push(op);
       buckets.ten.push(op);
     } else if (moment(track.addedAt).isSameOrAfter(twentyMinutes)) {
       buckets.twenty.push(op);
     } else {
       buckets.thirty.push(op);
     }
-  });
+    return buckets;
+  }, {ten: [], twenty: [], thirty: []});
 
-  const groups = [];
-  Object.keys(buckets).forEach((key) => {
+  const groups = Object.keys(buckets).reduce((groups, key) => {
     if (buckets[key].length) {
       switch (key) {
         case `ten`:
-          groups.push(optionGroup(`Added less than 10 minutes ago`, buckets[key]));
+          groups.push(optionGroup(LABELS.ten, buckets[key]));
           break;
         case `twenty`:
-          groups.push(optionGroup(`Added less than 20 minutes ago`, buckets[key]));
+          groups.push(optionGroup(LABELS.twenty, buckets[key]));
           break;
         default:
-          groups.push(optionGroup(`Added less than 30 minutes ago`, buckets[key]));
+          groups.push(optionGroup(LABELS.thirty, buckets[key]));
           break;
       }
     }
-  });
+    return groups;
+  }, []);
   const blocks = [
-    textSection(RESET_RESPONSE.review_title(playlistTracks.length)),
-    multiSelectStaticGroups(RESET_MODAL, `Select songs to keep on the playlist`, `Tracks added in the past 10 minutes have been pre-selected.`, initialOptions.length ? initialOptions : null, groups, true),
-    selectStatic(REVIEW_JUMP, `Jump to the start of the playlist?`, `This will only work if a track is selected above.`, option(`Yes`, 'true'), yesOrNo()),
+    textSection(RESPONSE.review_title(playlistTracks.length)),
+    multiSelectStaticGroups(RESET_MODAL, LABELS.select, LABELS.preselect, buckets.ten.length ? buckets.ten : null, groups, true),
+    selectStatic(REVIEW_JUMP, LABELS.jump, LABELS.jump_desc, option(`Yes`, 'true'), yesOrNo()),
   ];
   return blocks;
 };
 
-const openResetReview = async (teamId, channelId, settings, viewId, responseUrl) => {
+const main = async (teamId, channelId, settings, viewId, responseUrl) => {
   // Delete review confirmation block
   const message = deleteReply('', null);
-  await reply(message, responseUrl);
+  reply(message, responseUrl);
+
   const auth = await authSession(teamId, channelId);
   const playlist = settings[PLAYLIST];
   const {total} = await fetchPlaylistTotal(auth, playlist.id);
   const reviewTracks = await getReviewTracks(auth, playlist, total);
   const blocks = getReviewBlocks(reviewTracks);
-  const metadata = channelId;
-  const view = slackModal(RESET_MODAL, `Reset: Review Tracks`, `Confirm`, `Close`, blocks, true, metadata);
+  const view = slackModal(RESET_MODAL, `Reset: Review Tracks`, `Confirm`, `Close`, blocks, true, channelId);
   await updateModal(viewId, view);
 };
 
 module.exports.handler = async (event, context) => {
   const {teamId, channelId, settings, viewId, responseUrl} = JSON.parse(event.Records[0].Sns.Message);
-  await openResetReview(teamId, channelId, settings, viewId, responseUrl)
+  await main(teamId, channelId, settings, viewId, responseUrl)
       .catch((error)=>{
-        logger.error(error, RESET_RESPONSE.failed);
-        reportErrorToSlack(teamId, channelId, null, RESET_RESPONSE.failed);
+        logger.error(error, RESPONSE.failed);
+        reportErrorToSlack(teamId, channelId, null, RESPONSE.failed);
       });
 };
+module.exports.RESPONSE = RESPONSE;
+module.exports.LABELS = LABELS;
 
