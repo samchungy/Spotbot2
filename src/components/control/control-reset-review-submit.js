b50a48cb5679dbb5c6de2f165290b8a61f295ec1
Sam Chung <schung2@deloitte.com.au>
2020-08-16 14:34:09 +1000

    add reset review submit unit tests

--- src/components/control/control-reset-review-submit.js
+++ src/components/control/control-reset-review-submit.js
@@ -9,7 +9,7 @@ const RESET_MODAL = config.slack.actions.reset_modal;
 const REVIEW_JUMP = config.slack.actions.reset_review_jump;
 const CONTROL_RESET_SET = process.env.SNS_PREFIX + 'control-reset-set';
 
-const RESET_RESPONSE = {
+const RESPONSE = {
   failed: 'Reset failed',
   error: ':warning: An error occured.',
 };
@@ -37,7 +37,7 @@ const extractSubmissions = (view) => {
   return submissions;
 };
 
-const reviewReset = async (teamId, channelId, settings, view, userId) => {
+const main = async (teamId, channelId, settings, view, userId) => {
   // Slack Modal was submitted. Keep whatever tracks were selected
   const {[RESET_MODAL]: trackUris, [REVIEW_JUMP]: jump} = extractSubmissions(view);
   const params = {
@@ -50,9 +50,10 @@ const reviewReset = async (teamId, channelId, settings, view, userId) => {
 
 module.exports.handler = async (event, context) => {
   const {teamId, channelId, settings, view, userId} = JSON.parse(event.Records[0].Sns.Message);
-  await reviewReset(teamId, channelId, settings, view, userId)
+  await main(teamId, channelId, settings, view, userId)
       .catch((error)=>{
-        logger.error(error, RESET_RESPONSE.failed);
-        reportErrorToSlack(teamId, channelId, null, RESET_RESPONSE.failed);
+        logger.error(error, RESPONSE.failed);
+        reportErrorToSlack(teamId, channelId, null, RESPONSE.failed);
       });
 };
+module.exports.RESPONSE = RESPONSE;
