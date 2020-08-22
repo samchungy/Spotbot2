6cb2db9d704b25c207acf9c1f139196ac1e976d2
Sam Chung <schung2@deloitte.com.au>
2020-08-22 23:09:07 +1000

    add get items unit tests

--- src/components/tracks/tracks-find-artists-get-artists.js
+++ src/components/tracks/tracks-find-artists-get-artists.js
@@ -6,7 +6,7 @@ const {showResults} = require('./layers/get-artists');
 // Slack
 const {reportErrorToSlack} = require('/opt/slack/slack-error-reporter');
 
-const TRACK_RESPONSE = {
+const RESPONSE = {
   failed: 'Fetching more search results failed',
 };
 
@@ -14,7 +14,8 @@ module.exports.handler = async (event, context) => {
   const {teamId, channelId, userId, triggerId, responseUrl} = JSON.parse(event.Records[0].Sns.Message);
   await showResults(teamId, channelId, userId, triggerId, responseUrl)
       .catch((error)=>{
-        logger.error(error, TRACK_RESPONSE.failed);
-        reportErrorToSlack(teamId, channelId, userId, TRACK_RESPONSE.failed);
+        logger.error(error, RESPONSE.failed);
+        reportErrorToSlack(teamId, channelId, userId, RESPONSE.failed);
       });
 };
+module.exports.RESPONSE = RESPONSE;
